f51a90887c0ba4234e189444f32458f207a5cee1
Aleksandr <100157630+SashaCurry@users.noreply.github.com>
2023-12-01 04:57:20 +0600

    Сделан 1ый пункт

--- NTMC_4/NTMC_4.cpp
+++ NTMC_4/NTMC_4.cpp
@@ -1,57 +1,48 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
 using namespace std;
+using namespace boost::multiprecision;
 
 class Factorization
 {
 private:
-	static int binaryEuclid(int a, int b)
-	{
+	static cpp_int usualEuclid(cpp_int a, cpp_int b) {
+		if (a < b)
+			swap(a, b);
 		if (a < 0 || b < 0)
 			throw string{ "Выполнение невозможно: a < 0 или b < 0" };
-
-		if (a == 0)
-			return b;
-		else if (b == 0 || a == b)
+		else if (b == 0)
 			return a;
-		else if (a == 1 || b == 1)
-			return 1;
-		else if ((a & 1) == 0 && (b & 1) == 0)
-			return binaryEuclid(a >> 1, b >> 1) << 1;
-		else if ((a & 1) == 0 && (b & 1) == 1)
-			return binaryEuclid(a >> 1, b);
-		else if ((a & 1) == 1 && (b & 1) == 0)
-			return binaryEuclid(a, b >> 2);
-		else {
-			if (b > a)
-				return binaryEuclid((b - a) >> 1, a);
-			else
-				return binaryEuclid((a - b) >> 1, b);
-		}
+
+		cpp_int r = a % b;
+		return usualEuclid(b, r);
 	}
 
 public:
-	static long long roMethodPollarda(long long n, double epsilon)
+	static cpp_int roMethodPollarda(cpp_int n, cpp_dec_float_50 eps)
 	{
-		long long T = round(sqrt(2 * sqrt(n) * log(1 / epsilon))) + 1;
-		vector <long long> xs;
-
+		cpp_int T = sqrt(2 * sqrt(n) * cpp_int(log(1 / eps))) + 1;
+		vector <cpp_int> xs;
+		
 	gen_x0:
+		cpp_int xCur = rand() % (n - 1) + 1, xNext;
 		xs.clear();
-		xs.push_back(rand() % (n - 1) + 1);
-		long long xCur = xs.back(), xNext;
+		xs.push_back(xCur);
 
 		for (unsigned short i = 1; i <= T; i++)
 		{
 			xNext = (xCur * xCur + 1) % n;
-			long long dk;
+			cpp_int dk;
 			bool allNot_1 = true;
 
-			for (unsigned short k = 0 ; k < xs.size(); k++)
+			for (unsigned short k = 0; k < xs.size(); k++)
 			{
-				dk = binaryEuclid(xNext - xs[k], n);
+				dk = usualEuclid((xNext - xs[k] + n) % n, n);
 				if (dk == n)
 					goto gen_x0;
 				else if (dk == 1)
@@ -73,9 +64,27 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
-	cout << "\tФакторизация целых чисел \n1 - Факторизация p-методом Полларда \n";
-	cout << "2 - Факторизация (р-1)-методом Полларда \n3 - Факторизация методом цепных дробей"; 
 
-	long long res = Factorization::roMethodPollarda(12345, 0.1);
-	cout << endl << "12345 = " << res << " * " << 12345 / res;
+	for (;;)
+	{
+		cout << "\tФакторизация целых чисел \n1 - Факторизация p-методом Полларда \n";
+		cout << "2 - Факторизация (р-1)-методом Полларда \n3 - Факторизация методом цепных дробей \n";
+		unsigned short x;
+		cin >> x;
+
+		if (x == 1)
+		{
+			cpp_int n;
+			cpp_dec_float_50 eps;
+			cout << "\nСоставное число n: ";
+			cin >> n;
+			cout << "Значение eps, 0 < eps < 1: ";
+			cin >> eps;
+
+			cpp_int res = Factorization::roMethodPollarda(n, eps);
+			cout << endl << n << " = " << res << " * " << n / res;
+		}
+
+		cout << "\n\n";
+	}
 }
